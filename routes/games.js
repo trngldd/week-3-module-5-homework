598923832382f2b98103f58486ccc16d7bc5f7ea
trngldd <145783506+trngldd@users.noreply.github.com>
2024-04-29 20:16:06 +0300

    Comment to commit

---
+++ routes/games.js
@@ -0,0 +1,11 @@
+     // routes/games.js
+     const gamesRouter = require("express").Router(); // Создали роутер
+     const { sendAllGames, deleteGame, addGameController } = require("../controllers/games")
+     const { getAllGames } = require("../middlewares/games")
+     
+     gamesRouter.get("/games", getAllGames, sendAllGames)
+     gamesRouter.delete("/games/:id", getAllGames, deleteGame);
+     gamesRouter.post("/games", getAllGames, addGameController) 
+     
+     module.exports = gamesRouter;
+     
