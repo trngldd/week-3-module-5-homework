8fb00d9901b868e272ad2d80d68c9a1033fc7b0c
trngldd <145783506+trngldd@users.noreply.github.com>
2024-05-16 19:23:38 +0300

    adjusting to checklist

--- routes/games.js
+++ routes/games.js
@@ -1,7 +1,7 @@
      // routes/games.js
      const gamesRouter = require("express").Router(); // Создали роутер
-     const { sendAllGames, deleteGame, addGameController } = require("../controllers/games")
-     const { getAllGames } = require("../middlewares/games")
+     const { sendAllGames } = require("../controllers/games")
+     const { getAllGames, deleteGame, addGameController } = require("../middlewares/games")
      
      gamesRouter.get("/games", getAllGames, sendAllGames)
      gamesRouter.delete("/games/:id", getAllGames, deleteGame);
